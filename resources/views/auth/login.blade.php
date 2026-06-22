9a1a5c9282c61de8486851d0a6f9eef7a13be2be
DESKTOP-SFN4QMD\Mohamad Maraqa <maraqamohamad@outlook.sa>
2026-06-22 17:06:36 +0300

    asd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
     <title>
-        Material Dashboard 2 PRO by Creative Tim
+        فداء | تسجيل الدخول
     </title>
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css"
