177d101c602e534139ace47aab9e88dbae6d473f
DESKTOP-SFN4QMD\Mohamad Maraqa <maraqamohamad@outlook.sa>
2026-06-22 17:04:29 +0300

    asd

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,10 @@
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
                 <div class="copyright text-center text-sm text-muted text-lg-end">
-                    CREATED BY -PERFECT AGENCY. PREMIUM E-COMMERCE SOLUTIONS.
+                    صنع بعناية وحب، برمجة وتطوير شركة
+                    <a href="https://perfect-agency.com" class="text-muted font-weight-bold" target="_blank" rel="noopener">
+                        Perfect Agency
+                    </a>
                 </div>
             </div>
 {{--            <div class="col-lg-6">--}}
